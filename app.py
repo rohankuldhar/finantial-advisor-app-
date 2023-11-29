4a187db5a1ac6e584a833a328e38bdcbb16b2ecc
Rohan Kuldhar <110922391+rohankuldhar@users.noreply.github.com>
2023-11-29 16:30:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# financial advisor chatbot with gpt cache
+
 import streamlit as st
 from dotenv import load_dotenv
 
@@ -16,6 +18,21 @@ from langchain.prompts.chat import (
     HumanMessagePromptTemplate,
 )
 from langchain import PromptTemplate
+import time
+
+from gptcache import cache
+from gptcache.embedding import Onnx
+from gptcache.manager import CacheBase, VectorBase, get_data_manager
+from gptcache.similarity_evaluation.distance import SearchDistanceEvaluation
+
+onnx = Onnx()
+data_manager = get_data_manager(CacheBase("sqlite"), VectorBase("faiss", dimension=onnx.dimension))
+cache.init(
+    embedding_func=onnx.to_embeddings,
+    data_manager=data_manager,
+    similarity_evaluation=SearchDistanceEvaluation(),
+    )
+cache.set_openai_key()
 
 load_dotenv()
 
@@ -28,12 +45,11 @@ def main():
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = []
 
+    start_time = time.time()
 
     with st.form('chat_input_form'):
         query = st.text_input( "Ask your query:", placeholder= "Ask your query:", label_visibility='collapsed')
         submit = st.form_submit_button("Submit")
-
-
     if st.button("New Chat"):
         st.session_state.clear()
 
@@ -43,16 +59,23 @@ def main():
     if submit:
 
         st.session_state.messages.append({"role": "human", "content": query})
-        llm = ChatOpenAI(model="gpt-3.5-turbo",temperature=0.7)
+        llm = ChatOpenAI(model="gpt-3.5-turbo")
         memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
         template= """
 
         Do not generate user responses on your own and avoid repeating questions.
 
-        Yor are a helpful financial advisor chatbot designed to assist/help user with their financial needs. You can provide personalized advice on 
+        Yor are a helpful financial advisor chatbot designed to assist/help user with their financial needs.
+        You can provide personalized advice on 
         managing finances, loan repayment strategies, investment options, retirement planning, and other financial queries. 
         You are here to help user make informed decisions and achieve their financial goals. 
+
+        The major goal for you is to have conversation with the user about whatever financial concern or 
+        needs the user has and if the user gets satisfied with the financial requirements of the user based 
+        on the conversation, you should connect the user with a human representative. So basically you as a financial
+        advisor for a bank and your goal is to sell some sort of financial product of the bank (loan, insurance, health plan, etc)
+        to the user.
         {chat_history}
         """
 
@@ -68,17 +91,20 @@ def main():
         for chat in st.session_state.chat_history:
             memory.save_context({"input": chat["input"]}, {"output": chat["output"]})
 
-        query
-    
-
         with get_openai_callback() as cb:
             response = chain.run(query)
             print(cb)
 
-        response
-
         st.session_state.messages.append({"role": "ai", "content": response})
         st.session_state.chat_history.append({"input": query, "output": response})
+
+
+        st.write("Time consuming: {:.2f}s".format(time.time() - start_time))
+        for i, message in enumerate(st.session_state.chat_history):
+            
+                st.write('🧑- ',message['input'])
+                st.write('🤖- ',message['output'])
+        
 
 if __name__ == '__main__':
     main()
